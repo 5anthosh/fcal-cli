189768b3f00dff8ecaa0f5c05566a5f54b6b80fe
Santhosh <santhoshkumarr1096@gmail.com>
2019-10-14 23:17:27 +0530

    Setup Package

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import colors = require("colors");
+import readline = require("readline");
+import { Fcal, Type } from "fcal";
+
+function main() {
+  const fcal = new Fcal();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.setPrompt("fcal> ");
+  rl.prompt();
+  rl.on("line", line => {
+    if (line === ".exit") {
+      rl.close();
+    }
+    try {
+      const value = fcal.evaluate(line) as Type.Numberic;
+      // tslint:disable-next-line:no-console
+      console.log(value.print());
+    } catch (error) {
+      // tslint:disable-next-line:no-console
+      console.error(colors.red(`Error ${error.stack}`));
+    }
+    rl.prompt();
+  }).on("close", () => {
+    process.exit(0);
+  });
+}
+
+main();
